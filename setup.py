80b5659cd93b87425e936000d0b4f6f4352d059e
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2019-04-10 12:11:37 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 setup(
-    name='helperlib',    # This is the name of your PyPI-package.
-    version='1.0',                          # python versioneer
+    name='helperlib',    
+    version='1.0',                         
     url="https://github.com/applied-systems-biology/HelperLib",
     author="Anna Medyukhina",
-    packages=['helper_lib'],
     author_email='anna.medyukhina@gmail.com',
+    packages=['helper_lib'],
     license='BSD-3-Clause',
 
     install_requires=[
