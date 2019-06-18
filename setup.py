03cfee3b7b907319b7af849d56f76a8f2b49b4a0
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2019-06-18 16:17:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name='helperlib_img',    
+    name='helper_lib',    
     version='2.0',                         
     url="https://github.com/applied-systems-biology/HelperLib",
     author="Anna Medyukhina",
